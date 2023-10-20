e1bf8b7f7289af1e4576c932190ffa8f5330389a
Sam_76 <muhammadusamah76@gmail.com>
2023-10-20 08:25:11 +0700

    Last Commit

--- index.php
+++ index.php
@@ -40,22 +40,22 @@ class Animal
 
 //index : Show all data
 echo "Menampilkan seluruh data";
-$animals = new Animal(["sapi","Kucing","Ikan","Burung"]);
+$animals = new Animal(["Sapi","Kucing","Ikan","Burung"]);
 echo $animals->index();
 
 //Store : Add new data
 $animals->store("Kerbau");
-echo "Telah menambahkan hewan (Kerbau)";
+echo "Telah menambahkan hewan (Kerbau) dengan menggunakan array_push";
 echo $animals->index();
 
 //Update : Change old data to the new one
 $animals->update(0, "Kadal");
-echo "Merubah (Sapi) dengan (Kadal)";
+echo "Sudah merubah index ke 0 menjadi data yang baru (Kadal)";
 echo $animals->index();
 
 //Destroy: Selected data
 $animals->destroy(4);
-echo "Menghapus list urutan ke 5 (Kerbau)";
+echo "Menghapus list urutan ke 5 (Kerbau) dengan menggunakan unset";
 echo $animals->index();
 
 ?>
